8120396763c8f77c01c93a321a8f684c60f6c59c
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-06-05 14:09:14 +0200

    init v4

---
+++ packages/api-client/src/index.ts
@@ -0,0 +1,24 @@
+import type { ApiRoutes } from "@webdev-project/backend/routes";
+export * as BackendTypes from "@webdev-project/backend/schema";
+
+import { hc } from "hono/client";
+
+// create instance to inline type in build
+// https://hono.dev/docs/guides/rpc#compile-your-code-before-using-it-recommended
+export const client = hc<ApiRoutes>("");
+export type apiClient = typeof client;
+
+// export default (...args: Parameters<typeof hc>): apiClient =>
+//   hc<ApiRoutes>(...args);
+
+export type ErrorSchema = {
+  error: {
+    issues: {
+      code: string;
+      path: (string | number)[];
+      message?: string | undefined;
+    }[];
+    name: string;
+  };
+  success: boolean;
+};
