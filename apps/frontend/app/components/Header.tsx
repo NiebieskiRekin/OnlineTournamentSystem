bef745f81d6a2cee247073545f84ef2067af9035
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-06-08 00:22:17 +0200

    layout

--- apps/frontend/app/components/Header.tsx
+++ apps/frontend/app/components/Header.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import NavbarBreadcrumbs from './NavbarBreadcrumbs';
+import { Box, Typography } from '@mui/material';
 
 export default function Header() {
   return (
+    <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+      <Stack
+        direction="row"
+        spacing={1}
+        sx={{ justifyContent: 'flex-start', width: '90%' }}
+      >
+        <Typography variant="h4" component="h1" sx={{ color: 'text.primary' }}>
+          Online Tournament System
+        </Typography>
+      </Stack>
     <Stack
       direction="row"
       sx={{
@@ -18,5 +29,6 @@ export default function Header() {
     >
       <NavbarBreadcrumbs />
     </Stack>
+    </Box>
   );
 }
