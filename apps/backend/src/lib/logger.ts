abedc2803f128a5cb32fe3dd172e155236559b89
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-06-08 15:58:00 +0200

    logger

--- apps/backend/src/lib/logger.ts
+++ apps/backend/src/lib/logger.ts
@@ -1,17 +1,5 @@
 import winston from "winston";
-// import path from "path";
-// import fs from "fs";
 
-/**
- * https://github.com/winstonjs/winston
- * https://github.com/winstonjs/logform
- *
- * zapisywać log jako json
- * nadać timestamp
- * zawęzić logi względem kategorii (db, client request, mail, ...)
- * zawęzić logi do określonej kategorii (info, warn, error)
- * każdy log powinien precyzyjnie wskazywać linię kodu, a ich ciąg przebieg procedury
- */
 const logFormat = winston.format.printf(
   ({ level, message, category, timestamp, stack }) => {
     return JSON.stringify({
@@ -23,13 +11,6 @@ const logFormat = winston.format.printf(
     });
   }
 );
-
-// const logDir = path.join(process.cwd(), "apps/backend/logs");
-//
-// // Sprawdzam czy katalog na logi istnieje
-// if (!fs.existsSync(logDir)) {
-//   fs.mkdirSync(logDir, { recursive: true });
-// }
 
 const logger = winston.createLogger({
   level: "info",
@@ -54,12 +35,6 @@ const logger = winston.createLogger({
   ],
 });
 
-/**
- * @param category - Kategoria loga (np. `db`, `server`, `mail`)
- * @param level - Poziom loga (`info`, `warn`, `error`)
- * @param message - Treść wiadomości
- * @param error - Opcjonalnie: obiekt błędu
- */
 export function log(
   category: string,
   level: "info" | "warn" | "error",
@@ -69,10 +44,5 @@ export function log(
   const stackTrace = new Error().stack?.split("\n")[2].trim(); // Pobiera plik i linię kodu
   logger.log({ level, category, message, stack: stackTrace, error });
 }
-
-// dodatkowo na konsole dla Nas
-// if (process.env.NODE_ENV !== "production") {
-//   logger.add(new winston.transports.Console());
-// }
 
 export default logger;
